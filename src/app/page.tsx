3ad0ed6d2e054e9f3c0f41aec728fc2d02e73c95
leefordjudes <leefordjudes@gmail.com>
2023-11-26 12:31:38 +0530

    Image and Content shift - adjust network to use slow g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ export default function HomePage() {
   return (
     <div>
       Home Page
-      <div className='absolute -z-10 inset-0'>
+      {/* <div className='absolute -z-10 inset-0'>
         <Image
           src={homeImg}
           alt='car factory'
           fill
           style={{ objectFit: 'cover' }}
         />
+      </div> */}
+      <div className='flex flex-row'>
+        <Image src={homeImg} alt='car factory' width={600} height={500} />
+        <div>Buy our product</div>
       </div>
     </div>
   );
